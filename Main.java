d1e5db89b4b356198ea4c99003537ca2d57abc9a
Gabriele 'Gabryx' Concli <gabriele.concli@gmail.com>
2020-05-16 09:25:43 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+  static final int DIM_BUFFER = 5;
+  protected static int[] Buffer = new int[DIM_BUFFER]
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
